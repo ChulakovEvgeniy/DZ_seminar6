5b4976a5803fb95b008164eee82dd2fdbb3466b2
«ChulakovEvgeniy» <>
2023-04-13 23:18:19 +0300

    решены 2 задачи

---
+++ zadacha41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+int INput(string nameSpace)
+{
+    System.Console.WriteLine(nameSpace);
+    int per1 = Int32.Parse(Console.ReadLine());
+    return per1;
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int k = INput("Введите число");
+        array[i] = k;
+    }
+}
+
+int CountPositiv(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+int length = INput("Введите количество чисел");
+int[] masiv = new int[length];
+FillArray(masiv);
+Console.Write(string.Join(",", masiv));
+System.Console.WriteLine();
+Console.WriteLine($"Количество положительных чисел = {CountPositiv(masiv)}");
+
---
+++ zadacha43/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double INput(string nameSpace)
+{
+    System.Console.WriteLine(nameSpace);
+    double per1 = Convert.ToDouble(Console.ReadLine());
+    return per1;
+}
+
+double b1 = INput("начало первой линии");
+double k1 = INput("конец первой линии");
+double b2 = INput("начало второй линии");
+double k2 = INput("конец второй линии");
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+System.Console.WriteLine($"точка пересечения двух прямых({x},{y})");
